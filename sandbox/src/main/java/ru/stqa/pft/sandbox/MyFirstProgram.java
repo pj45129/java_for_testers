9155090b110697bc54d486f316cf9df50cfcb868
pj45129 <pj45129@gmail.com>
2020-05-10 16:13:49 +0300

    добавил тест

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -11,9 +11,12 @@ class MyFirstProgram {
 
         Rectangle r = new Rectangle(4, 6);
         System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
     }
 
     public static void hello(String somebody) {
         System.out.println("Hello, " + somebody + "!");
     }
+
+
 }
