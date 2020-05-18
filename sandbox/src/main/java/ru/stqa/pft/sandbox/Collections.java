f3e773836c5809eb19153cf2d197a44a726cd193
pj45129 <pj45129@gmail.com>
2020-05-18 14:53:05 +0300

    добавил классы с коллекцией, списком и циклом

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.sandbox;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Collections {
+
+    public static void main(String[] args) {
+        String[] langs = {"java", "C#", "Python", "PHP"};
+
+        List<String> languages = Arrays.asList("java", "C#", "Python", "PHP");
+
+
+        for (String l : languages) {
+            System.out.println("Я хочу выучить " + l);
+        }
+    }
+}
